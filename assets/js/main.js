7362bfcd29796eeaeffb9b10f2f8fc2ad601bb3e
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-04-07 22:02:35 -0300

    Inicia Js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const firstName = document.querySelector('#first_name')
+const lastName = document.querySelector('#last_name')
+const emailAdress = document.querySelector('#email_adress')
+const password = document.querySelector('#password')
+const buttonSubmit = document.querySelector('#button_submit')
+
+
