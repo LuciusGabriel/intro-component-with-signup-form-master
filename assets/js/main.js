e76c1133199dfbc9f25c6b97fea989a496c80363
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-04-18 19:57:42 -0300

    Continue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 const firstName = document.querySelector('#first_name')
 const firstNameSmall = document.querySelector('.first_name')
 const lastName = document.querySelector('#last_name')
+const lastNameSmall = document.querySelector('.last_name')
 const emailAdress = document.querySelector('#email_adress')
+const emailAdressSmall = document.querySelector('.email_adress')
 const password = document.querySelector('#password')
+const passwordSmall = document.querySelector('.password')
 const buttonSubmit = document.querySelector('#button_submit')
 
 
@@ -14,11 +17,17 @@ buttonSubmit.addEventListener('click', ()=>{
     }
     if(lastName.value === ''){
         console.log('Campo Sobrenome Vazio')
+        lastName.classList.add('required')
+        lastNameSmall.classList.remove('invisible')
     }
     if(emailAdress.value === ''){
         console.log('Campo Email Vazio')
+        emailAdress.classList.add('required')
+        emailAdressSmall.classList.remove('invisible')
     }
     if(password.value === ''){
         console.log('Campo Password Vazio')
+        password.classList.add('required')
+        passwordSmall.classList.remove('invisible')
     }
 })
